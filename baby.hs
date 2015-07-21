88583c8d2d2ed19b48981d59951fded60626e2e9
mitchthorson <mthorson@usatoday.com>
2015-07-20 23:54:40 -0400

    end of lesson 2

--- baby.hs
+++ baby.hs
@@ -5,6 +5,13 @@ doubleUs x y = x*2 + y*2
 testMatch 7 = "Got it!"
 testMatch x = "Missed!"
 
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
+boombang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
